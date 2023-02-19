b3c19118381fe9e729433cef80c33ebda8915904
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 22:43:25 +0900

    StyledComponents: Adapting & Extending

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+//styled components에 prop 적용하기.
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+//Extending 속성을 그대로 가져와서 다른 속성을 추가하고 싶을 때!
+/*
+const Circle = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+`;
+*/
+// 요롷게 extend!!
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgColor="tomato" />
+    </Father>
   );
 }
 
