5b7bf3bf88aa875332e8b6d4a142296aebff2faf
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-20 15:45:43 +0900

    event type

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import Circle from './Circle';
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
 
 function App() {
+  const [value, setValue] = useState('');
+
+  // event 타입 지정할 땐 구글링!
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    //TS는 e.currentTarget 사용하기로 정함
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('hello', value);
+  };
   return (
-    <Wrapper>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </Wrapper>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={value}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+    </div>
   );
 }
 
